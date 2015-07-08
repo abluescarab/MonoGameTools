89c4e31d2bcdce53248fa6fdda8d8b83dcf1a62b
abluescarab <alana@abluescarab.us>
2015-07-08 01:05:15 -0700

    Added demo project

---
+++ MonoGameToolsDemo/DemoScreen.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Tools;
+using MonoGame.Tools.Components;
+using MonoGame.Tools.Effects;
+using MonoGame.Tools.Transitions;
+
+namespace MonoGameToolsDemo {
+    public class DemoScreen : GameScreen {
+        public Dictionary<string, Image> Images { get; private set; }
+
+        public DemoScreen() {
+            Images = new Dictionary<string, Image>();
+
+            Image mainImage = new Image("MonoGameTools", new Vector2(10, 10),
+                Vector2.One, centerOrigin: false);
+            Image fadeImage = new Image("FadeEffect", new Vector2(30, 60),
+                Vector2.One, false, centerOrigin: false);
+            Image flashImage = new Image("FlashEffect", new Vector2(30, 110),
+                Vector2.One, false, centerOrigin: false);
+            Image zoomImage = new Image("ZoomEffect", new Vector2(30, 160),
+                Vector2.One, false, centerOrigin: false);
+
+            fadeImage.AddEffect<FadeEffect>("fadeEffect", new FadeEffect(
+                repeat: true));
+            flashImage.AddEffect<FlashEffect>("flashEffect", new FlashEffect(
+                0.5f, minimumAlpha: .25f, repeat: true));
+            zoomImage.AddEffect<ZoomEffect>("zoomEffect", new ZoomEffect(
+                new Vector2(0.25f, 0.25f), new Vector2(1.0f, 1.0f),
+                repeat: true));
+
+            Images.Add("mainImage", mainImage);
+            Images.Add("fadeImage", fadeImage);
+            Images.Add("flashImage", flashImage);
+            Images.Add("zoomImage", zoomImage);
+        }
+
+        public override void LoadContent(ContentManager content) {
+            base.LoadContent(content);
+
+            foreach(Image image in Images.Values) {
+                image.LoadContent(content);
+            }
+        }
+
+        public override void UnloadContent() {
+            base.UnloadContent();
+
+            Images["fadeImage"].Visible = false;
+            Images["flashImage"].Visible = false;
+            Images["zoomImage"].Visible = false;
+
+            foreach(Image image in Images.Values) {
+                image.UnloadContent();
+            }
+        }
+
+        public override void Update(GameTime gameTime) {
+            base.Update(gameTime);
+
+            foreach(Image image in Images.Values) {
+                image.Update(gameTime);
+            }
+
+            if(!ScreenManager.Instance.IsTransitioning) {
+                if(InputManager.Instance.KeyPressed(Keys.Enter) ||
+                    InputManager.Instance.ButtonPressed(PlayerIndex.One,
+                    Buttons.A) || InputManager.Instance.MouseButtonPressed(
+                    InputManager.MouseButton.Right)) {
+                    if(!Images["fadeImage"].Visible) {
+                        Images["fadeImage"].Visible = true;
+                        Images["fadeImage"].ActivateEffect("fadeEffect");
+                    }
+                    else {
+                        if(!Images["flashImage"].Visible) {
+                            Images["flashImage"].Visible = true;
+                            Images["flashImage"].ActivateEffect("flashEffect");
+                        }
+                        else {
+                            if(!Images["zoomImage"].Visible) {
+                                Images["zoomImage"].Visible = true;
+                                Images["zoomImage"].ActivateEffect("zoomEffect");
+                            }
+                            else {
+                                ScreenManager.Instance.ChangeScreen(this,
+                                    new FadeTransition(ScreenManager.Instance.Dimensions,
+                                        Color.Black));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch) {
+            foreach(Image image in Images.Values) {
+                if(image.Visible) {
+                    image.Draw(spriteBatch);
+                }
+            }
+        }
+    }
+}
---
+++ MonoGameToolsDemo/Game1.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Tools;
+using MonoGame.Tools.Components;
+using MonoGame.Tools.Effects;
+using MonoGame.Tools.Transitions;
+
+namespace MonoGameToolsDemo {
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        public Game1() {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize() {
+            graphics.PreferredBackBufferWidth = (int)ScreenManager.Instance.Dimensions.X;
+            graphics.PreferredBackBufferHeight = (int)ScreenManager.Instance.Dimensions.Y;
+            graphics.ApplyChanges();
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent() {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            ScreenManager.Instance.GraphicsDevice = GraphicsDevice;
+            ScreenManager.Instance.SpriteBatch = spriteBatch;
+            ScreenManager.Instance.LoadContent(Content);
+
+            ScreenManager.Instance.AddScreen<DemoScreen>("demoScreen",
+                new DemoScreen());
+            ScreenManager.Instance.AddScreen<DemoScreen>("demoScreen2",
+                new DemoScreen());
+
+            ScreenManager.Instance.ChangeScreen(
+                ScreenManager.Instance.FindScreenByName("demoScreen"));
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent() {
+            ScreenManager.Instance.UnloadContent();
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime) {
+            if(GamePad.GetState(PlayerIndex.One).Buttons.Back == 
+                ButtonState.Pressed || 
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            ScreenManager.Instance.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            ScreenManager.Instance.Draw(spriteBatch);
+            base.Draw(gameTime);
+        }
+    }
+}
