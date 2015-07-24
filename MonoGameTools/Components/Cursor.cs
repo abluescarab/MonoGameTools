44e1091c98029357a1be6e2f638be9ccf6b00d28
abluescarab <alana@abluescarab.us>
2015-07-23 20:40:22 -0700

    Added Cursor class

--- MonoGameTools/Components/Cursor.cs
+++ MonoGameTools/Components/Cursor.cs
@@ -10,16 +10,34 @@ namespace MonoGame.Tools {
         private string imagePath = string.Empty;
         private Image image = null;
 
+        /// <summary>
+        /// The position of the cursor.
+        /// </summary>
         public Vector2 Position { get; private set; }
+        /// <summary>
+        /// Whether the cursor is visible.
+        /// </summary>
         public bool Visible { get; set; }
+        /// <summary>
+        /// The scale of the cursor.
+        /// </summary>
         public Vector2 Scale { get; set; }
 
+        /// <summary>
+        /// Initialize the cursor.
+        /// </summary>
+        /// <param name="image">The image to use for the cursor</param>
         public Cursor(string image) {
             imagePath = image;
             Visible = true;
             Scale = Vector2.One;
         }
 
+        /// <summary>
+        /// Initialize the cursor.
+        /// </summary>
+        /// <param name="image">The image to use for the cursor</param>
+        /// <param name="position">The initial location of the cursor</param>
         public Cursor(string image, Vector2 position) {
             imagePath = image;
             Mouse.SetPosition((int)position.X, (int)position.Y);
@@ -27,6 +45,10 @@ namespace MonoGame.Tools {
             Scale = Vector2.One;
         }
 
+        /// <summary>
+        /// Load the Cursor content.
+        /// </summary>
+        /// <param name="content">The ContentManager to load to</param>
         public void LoadContent(ContentManager content) {
             if(!string.IsNullOrEmpty(imagePath)) {
                 image = new Image(imagePath, Position, Scale, centerOrigin: false);
@@ -37,12 +59,19 @@ namespace MonoGame.Tools {
             }
         }
 
+        /// <summary>
+        /// Unload the Cursor content.
+        /// </summary>
         public void UnloadContent() {
             if(image != null) {
                 image.UnloadContent();
             }
         }
 
+        /// <summary>
+        /// Update the Cursor content.
+        /// </summary>
+        /// <param name="gameTime">The game time TimeSpan</param>
         public void Update(GameTime gameTime) {
             if(image != null) {
                 Vector2 pos = Mouse.GetState().Position.ToVector2();
@@ -73,6 +102,10 @@ namespace MonoGame.Tools {
             }
         }
 
+        /// <summary>
+        /// Draw the Cursor.
+        /// </summary>
+        /// <param name="spriteBatch">The SpriteBatch to draw to</param>
         public void Draw(SpriteBatch spriteBatch) {
             if(image != null) {
                 image.Draw(spriteBatch);
